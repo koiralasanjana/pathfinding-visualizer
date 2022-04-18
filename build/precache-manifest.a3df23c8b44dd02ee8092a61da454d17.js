580c7b495976fe6b18a68e99ab1f0ff2c77ac38a
sanjanakoirala <sanjana.koirala@github.com>
2022-04-18 23:25:16 +0545

    initial commit

---
+++ build/precache-manifest.a3df23c8b44dd02ee8092a61da454d17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5bf2ce2e1f5a0182e8d80c6da0f245",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad3099c61b77ea1caef9",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "bedbcc144dedc1729327",
+    "url": "/static/css/main.f0ba43f6.chunk.css"
+  },
+  {
+    "revision": "ad3099c61b77ea1caef9",
+    "url": "/static/js/2.de969b78.chunk.js"
+  },
+  {
+    "revision": "38e6a860e355c4777cf04ab30df5a2db",
+    "url": "/static/js/2.de969b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bedbcc144dedc1729327",
+    "url": "/static/js/main.599e022e.chunk.js"
+  },
+  {
+    "revision": "7eae23df8283499875ca",
+    "url": "/static/js/runtime-main.56d0fca6.js"
+  }
+]);
